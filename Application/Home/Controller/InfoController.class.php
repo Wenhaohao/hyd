baa218c7d738bce22e86a99038c780fe396e873a
wen_hao <min.wen@jingubang.com>
2014-10-21 09:43:04 +0800

    d添加目录

---
+++ Application/Home/Controller/InfoController.class.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author:  min_wenhao<min_Wenhao@163.com>
+// +----------------------------------------------------------------------
+// | 2014-10-12
+// +----------------------------------------------------------------------
+
+/**
+ * 用户中心控制器
+ */
+namespace Home\Controller;
+use Think\Controller;
+
+class InfoController extends Controller {
+	
+	public function index(){
+		$this->display();
+	}
+	
+}
