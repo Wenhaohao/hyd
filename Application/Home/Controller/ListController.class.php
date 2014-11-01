9df23bcb2bc0e0896e483467014e5e4a82cc335a
wenhaohao <397173001@qq.com>
2014-11-01 19:39:44 +0800

    list

--- Application/Home/Controller/ListController.class.php
+++ Application/Home/Controller/ListController.class.php
@@ -16,11 +16,33 @@ use Think\Controller;
 class ListController extends Controller {
 	
 	public function index(){
+		$listService = D('List','Service');
+		
+		// 翻页
+		$listCount = $listService->getCount();
+		$page = new \Think\Page($listCount,2);
+		$first = $page->firstRow;
+		$last = $page->listRows;
+		$list = $page->show();
+		$listData = $listService->getList($first,$last);
+		
+		// 分类
+		$categoryData = $listService->getCategorys();
+// 		foreach ($categoryData as $key => $val){
+// 			if($val['parent_id'] == null){
+// 				$data[] = $val;
+// 			}
+			
+// 		}
+// 		dump($data);
+// 		exit();
+		$this->assign('category',$categoryData);
+		$this->assign('list',$listData);
+		$this->assign('page',$list);
 		$this->display();
 	}
 
 	public function article(){
-
 		$this->display();
 	}
 	
